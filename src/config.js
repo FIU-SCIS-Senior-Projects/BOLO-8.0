d84240291d533625c6d978c774f77fd4196ae82e
acorr092 <acorr092@fiu.edu>
2017-06-15 19:18:50 -0400

    modify .config file

--- src/config.js
+++ src/config.js
@@ -40,6 +40,9 @@ config.max_age = 1000 * 15 * 60;
 config.maxNumberOfLogInAttempts = 10;
 
 //Database configuration
+console.log('NODE ENV', process.env.NODE_ENV);
+console.log('MONGO URI', process.env.MONGO_URI);
+
 config.db = process.env.NODE_ENV === 'develop' ?
   'mongodb://localhost:27017/BOLO' :
   process.env.MONGO_URI;
